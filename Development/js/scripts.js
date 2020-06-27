7dfdb786520bf79ced059a649cc28da19e32bcfe
فاطمه محمد حسن- 474 <66556517+fmfah2011@users.noreply.github.com>
2020-06-27 20:40:39 +0300

    تم حل الواجب

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
-// 1
+// 1 
+const name= "fatma";
 
 // 2
-
+let number=16;
 // 3 and 4
-
+console.log(name + " " + number)
 // 5 and 6
+console.log(number *5)
+number = number*3
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hobbies =["reading","art","eating","photography"]
 // 2
-
+hobbies.push(number)
+console.log (hobbies.length)
 // 3
-
+hobbies.pop(number)
+console.log (hobbies)
 // 4
-
+let student = { road:"web", lang:"css"};
 // 5
-
+student.ta=["rahaf","nancy"]
 // 6
-
+console.log(student.road);
 // 7
-
+console.log("rahaf")
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
